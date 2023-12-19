aa86403052c98f6485c5d8244f23d740bcc1b98b
i3oi3ka <i3oi3ka@ukr.net>
2023-12-19 23:05:41 +0200

    add permission for change user

--- core_apps/accounts/views.py
+++ core_apps/accounts/views.py
@@ -1,13 +1,22 @@
 import django.contrib.auth
 import django_filters
 from rest_framework import status, generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser, BasePermission, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from core_apps.accounts.models import User
 from core_apps.accounts.serializers import UserSerializer, UserCreateSerializer, ChangePasswordSerializer
+
+
+class IsOwnerOrAdminPermission(BasePermission):
+    """
+    Custom permission to only allow owners or admins to access an object.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Check if the user is an admin or the owner of the object
+        return request.user.is_authenticated and (request.user.is_staff or request.user == obj)
 
 
 # Create your views here.
@@ -27,6 +36,7 @@ class UserView(ModelViewSet):
     queryset = User.objects.all().prefetch_related('settings')
     serializer_class = UserSerializer
     filterset_class = UserFilter
+
     # permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
@@ -47,6 +57,18 @@ class UserView(ModelViewSet):
         }
 
         return Response(response_data, status=status.HTTP_201_CREATED)
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        if self.action == 'retrieve' or self.action == 'list':
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsOwnerOrAdminPermission]
+        return [permission() for permission in permission_classes]
 
     def get_serializer_class(self, *args, **kwargs):
         if self.action == 'create':
