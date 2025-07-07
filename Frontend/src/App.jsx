edf5c8c84534c0db1f8c8a373186c111ac7ac1f5
i3oi3ka <i3oi3ka@ukr.net>
2025-07-07 23:25:31 +0300

    feat: add routing with react-router and create HomePage, LoginPage, and RegistrationPage components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import RegistationPage from "./pages/RegistationPage/RegistationPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
-
-
   return (
     <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegistationPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
