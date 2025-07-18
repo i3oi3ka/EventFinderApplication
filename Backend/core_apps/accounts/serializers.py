f6501a6e7448c638fdfcbafdb13f9a905055ead8
i3oi3ka <i3oi3ka@ukr.net>
2025-07-18 23:42:03 +0300

    feat: refactor code style for consistency; implement CustomTokenRefreshView and update serializers

--- Backend/core_apps/accounts/serializers.py
+++ Backend/core_apps/accounts/serializers.py
@@ -2,6 +2,8 @@ import django.contrib.auth
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 
 from core_apps.accounts.models import Settings, User
 
@@ -9,7 +11,7 @@ from core_apps.accounts.models import Settings, User
 class SettingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Settings
-        fields = '__all__'
+        fields = "__all__"
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
@@ -17,30 +19,40 @@ class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True,
         required=True,
-        style={'input_type': 'password', 'placeholder': 'Password'},
-        validators=[validate_password]
+        style={"input_type": "password", "placeholder": "Password"},
+        validators=[validate_password],
     )
     password2 = serializers.CharField(
         write_only=True,
         required=True,
-        style={'input_type': 'password', 'placeholder': 'Password'},
+        style={"input_type": "password", "placeholder": "Password"},
     )
 
     class Meta:
         model = django.contrib.auth.get_user_model()
-        fields = ['id', 'nickname', 'username', 'email', 'password', 'password2', 'settings']
+        fields = [
+            "id",
+            "nickname",
+            "username",
+            "email",
+            "password",
+            "password2",
+            "settings",
+        ]
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
         return attrs
 
     def create(self, validated_data):
-        settings_data = validated_data.pop('settings', None)
-        validated_data['password'] = make_password(validated_data.pop('password2'))
+        settings_data = validated_data.pop("settings", None)
+        validated_data["password"] = make_password(validated_data.pop("password2"))
         if not settings_data:
             settings_data = {}
-        validated_data['settings'] = Settings.objects.create(**settings_data)
+        validated_data["settings"] = Settings.objects.create(**settings_data)
         return super(UserCreateSerializer, self).create(validated_data)
 
 
@@ -50,39 +62,56 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'nickname', 'first_name', 'last_name', 'email', 'username', 'email', 'settings']
+        fields = [
+            "id",
+            "nickname",
+            "first_name",
+            "last_name",
+            "email",
+            "username",
+            "email",
+            "settings",
+        ]
 
     def update(self, instance, validated_data):
-        settings_data = validated_data.pop('settings', None)
+        settings_data = validated_data.pop("settings", None)
         instance = super().update(instance, validated_data)
         if settings_data:
-            instance.settings = SettingsSerializer().update(instance.settings, settings_data)
+            instance.settings = SettingsSerializer().update(
+                instance.settings, settings_data
+            )
         instance.save()
         return instance
 
 
 class ChangePasswordSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(
+        write_only=True, required=True, validators=[validate_password]
+    )
     password2 = serializers.CharField(write_only=True, required=True)
     old_password = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ('old_password', 'password', 'password2')
+        fields = ("old_password", "password", "password2")
 
     def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError(
+                {"password": "Password fields didn't match."}
+            )
         return attrs
 
     def validate_old_password(self, value):
-        user = self.context['request'].user
+        user = self.context["request"].user
         if not user.check_password(value):
-            raise serializers.ValidationError({"old_password": "Old password is not correct"})
+            raise serializers.ValidationError(
+                {"old_password": "Old password is not correct"}
+            )
         return value
 
     def update(self, instance, validated_data):
-        instance.set_password(validated_data['password'])
+        instance.set_password(validated_data["password"])
         instance.save()
         return instance
 
@@ -90,4 +119,32 @@ class ChangePasswordSerializer(serializers.ModelSerializer):
 class ChangeUserToAdmin(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['is_staff', 'is_superuser']
+        fields = ["is_staff", "is_superuser"]
+
+
+class CustomTokenRefreshSerializer(TokenRefreshSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        refresh_token = attrs["refresh"]
+        token = RefreshToken(refresh_token)
+
+        try:
+            user = User.objects.get(id=token["user_id"])
+            print(f"User found: {token}")
+        except User.DoesNotExist:
+            raise serializers.ValidationError("User not found")
+
+        # Додаємо додаткову інформацію
+        data.update(
+            {
+                "user": {
+                    "id": user.id,
+                    "email": user.email,
+                    "username": user.username,
+                    # інші потрібні поля
+                }
+            }
+        )
+
+        return data
--- Backend/core_apps/accounts/urls.py
+++ Backend/core_apps/accounts/urls.py
@@ -4,17 +4,32 @@ from rest_framework.routers import DefaultRouter
 from .views import UserView, ChangePasswordView, ChangeUserToAdminView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView, TokenVerifyView,
+    TokenRefreshView,
+    TokenVerifyView,
 )
 
-router = DefaultRouter()
-router.register('', UserView, basename='accounts')
+from .views import CustomTokenRefreshView
 
-urlpatterns = [path('change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
-               path('change_user_to_admin/<int:pk>/', ChangeUserToAdminView.as_view(), name='change_user_to_admin'),
-               path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-               path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-               path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-               path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-               ]
+router = DefaultRouter()
+router.register("", UserView, basename="accounts")
+
+urlpatterns = [
+    path(
+        "change_password/<int:pk>/",
+        ChangePasswordView.as_view(),
+        name="auth_change_password",
+    ),
+    path(
+        "change_user_to_admin/<int:pk>/",
+        ChangeUserToAdminView.as_view(),
+        name="change_user_to_admin",
+    ),
+    path(
+        "password_reset/",
+        include("django_rest_passwordreset.urls", namespace="password_reset"),
+    ),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
 urlpatterns += router.urls
