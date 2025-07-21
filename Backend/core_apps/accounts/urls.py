43f4ff4cb105efb74f296346df8a2578c09bea91
i3oi3ka <i3oi3ka@ukr.net>
2025-07-21 22:17:29 +0300

    feat: replace TokenObtainPairView with CustomTokenObtainPairView in URL routing

--- Backend/core_apps/accounts/urls.py
+++ Backend/core_apps/accounts/urls.py
@@ -9,7 +9,7 @@ from rest_framework_simplejwt.views import (
 )
 from django.contrib.auth.views import LogoutView
 
-from .views import CustomTokenRefreshView
+from .views import CustomTokenRefreshView, CustomTokenObtainPairView
 
 
 router = DefaultRouter()
@@ -30,7 +30,7 @@ urlpatterns = [
         "password_reset/",
         include("django_rest_passwordreset.urls", namespace="password_reset"),
     ),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("logout/", LogoutView.as_view(), name="token_logout"),
